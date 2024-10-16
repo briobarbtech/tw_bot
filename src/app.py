4202992de51b17539735c0020b652c75f76906fc
Brian Barquesi <barquesi.tech@gmail.com>
2024-10-16 12:58:17 -0300

    (feat) I improved the publish function by automating the content selection process, optimizing the decision-making on what to publish based on predefined criteria.

--- src/app.py
+++ src/app.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv
 import random
 import time
 from datetime import datetime, timedelta
+from PIL import Image
 
 dotenv_path = join(dirname(__file__), '../.env')
 load_dotenv(dotenv_path)
-
+dir= 'images/'
 
 # Autenticación
 api_key = os.getenv('API_KEY')
@@ -17,6 +18,13 @@ access_token = os.getenv('ACCESS_TOKEN')
 access_token_secret = secret=os.getenv('ACCESS_TOKEN_SECRET')
 bearer_token=os.getenv('BEARER_TOKEN')
 
+def loadImages(folder):
+    images = {}
+    for archivo in os.listdir(f"{dir}cap_{folder}"):
+        if archivo.endswith(('jpg', 'png', 'jpeg')):
+            img = Image.open(os.path.join(f"{dir}cap_{folder}", archivo))
+            images[archivo] = img
+    return images
 
 def authentication():
     try:
@@ -26,26 +34,39 @@ def authentication():
     except tweepy.TweepyException as e:
         print(f"Error de autenticación: {e}")
 
-def publish(id):
+def publish(content):
     try:
         client = tweepy.Client(bearer_token,api_key,api_secret,access_token,access_token_secret) 
         api = authentication()
-        image_path = f'./images/image({id}).png'
+        image_path = content[0]
         media = api.media_upload(image_path)
         media_id = media.media_id_string
-        client.create_tweet(text=f'image {id}/100',media_ids=[media_id])
+        client.create_tweet(text=f'image {id}/100 \n\n Link: {content[1]}',media_ids=[media_id])
        
     except tweepy.TweepyException as e:
         print(f"Error: {e}")
     finally:
         print("Se ha completado la tarea")
+def lengthFolder(dir):
+    folders = [nombre for nombre in os.listdir(dir) if os.path.isdir(os.path.join(dir, nombre))]
+    length_folders = len(folders)
+    return length_folders
+
+def chooseContent():
+    length_folder = lengthFolder(dir)
+    choosed_folder = random.randint(1,length_folder)
+    length_images = len(loadImages(choosed_folder))
+    choosed_image = random.randint(1,length_images)
+    images = f"{dir}cap_{choosed_folder}/image ({choosed_image})"
+    link = f"https://www.webtoons.com/es/canvas/contrast-resistance/tren-y-resistencia/viewer?title_no=516436&episode_no={choosed_folder}"
+    return [images,link]
+
 
 def main():
     while True:
         now = datetime.now() - timedelta(hours=0)
         if now.hour == 20 and now.minute == 9:
-            publish(random.randint(0,100))
-            publish(1)
+            publish(chooseContent())
             time.sleep(60)
         time.sleep(1)
 
@@ -58,4 +79,5 @@ def publish_test():
         print(f"Error: {e}")
     finally:
         print("Se ha completado la tarea")
-publish_test()
+#publish_test()
+
